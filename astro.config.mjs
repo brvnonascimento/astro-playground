64150a3fada63983af1c2663ad3ff94fffe59512
Bruno Nascimento <bruno.hn@icloud.com>
2023-03-09 19:45:28 -0300

    chore: remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,28 +5,32 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import storyblok from "@storyblok/astro";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+
 import { loadEnv } from "vite";
-import vercel from "@astrojs/vercel/serverless";
+
 const env = loadEnv("", process.cwd(), "PUBLIC");
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   vite: {
-    plugins: [basicSsl()]
+    plugins: [basicSsl()],
   },
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx(), sitemap(), storyblok({
-    accessToken: env.PUBLIC_STORYBLOK_ACCESS_TOKEN,
-    components: {
-      page: "storyblok/page",
-      hero: "storyblok/hero"
-    },
-    apiOptions: {
-      region: "us"
-    }
-  })],
-  output: "server",
-  adapter: vercel()
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+    sitemap(),
+    storyblok({
+      accessToken: env.PUBLIC_STORYBLOK_ACCESS_TOKEN,
+      components: {
+        page: "storyblok/page",
+        hero: "storyblok/hero",
+      },
+      apiOptions: {
+        region: "us",
+      },
+    }),
+  ],
 });

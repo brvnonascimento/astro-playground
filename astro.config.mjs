50ea514e4ad9b9f296d21362edd2d721ec7867bf
Bruno Nascimento <bruno.hn@icloud.com>
2023-03-09 01:54:13 -0300

    feat: add page builder like editing integrated with Storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,18 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import storyblok from "@storyblok/astro";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 export default defineConfig({
   site: "https://astroship.web3templates.com",
+  vite: {
+    plugins: [basicSsl()],
+  },
   integrations: [
     tailwind(),
     image({
@@ -13,5 +22,15 @@ export default defineConfig({
     }),
     mdx(),
     sitemap(),
+    storyblok({
+      accessToken: env.STORYBLOK_ACCESS_TOKEN,
+      components: {
+        page: "storyblok/page",
+        hero: "storyblok/hero",
+      },
+      apiOptions: {
+        region: "us",
+      },
+    }),
   ],
 });

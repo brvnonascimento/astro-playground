5eb0d5bd05f0c5af33574f922e17573108dbf357
Bruno Nascimento <bruno.hn@icloud.com>
2023-03-09 21:24:45 -0300

    chore: get rid of unnecessary plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,18 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import storyblok from "@storyblok/astro";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-
-import { loadEnv } from "vite";
-
-const env = loadEnv("", process.cwd(), "PUBLIC");
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   vite: {
     plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
   },
   integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
     storyblok({
-      accessToken: env.PUBLIC_STORYBLOK_ACCESS_TOKEN,
+      accessToken: "qBlqZdNwjFb738diAycVNQtt",
       components: {
         page: "storyblok/page",
         hero: "storyblok/hero",

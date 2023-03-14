3901e68cb4adfd94680d2a37caf2fecc0bc7cb27
Bruno Nascimento <bruno.hn@icloud.com>
2023-03-14 20:51:19 -0300

    chore: remove vite plugin single file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import vercelServerless from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  vite: {
-    plugins: [viteSingleFile()],
-  },
   output: "server",
   adapter: vercelServerless(),
 });

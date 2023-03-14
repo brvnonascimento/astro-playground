47260ec29fd0dbb8e1ebbb41d6e05a5a0631e377
Bruno Nascimento <bruno.hn@icloud.com>
2023-03-14 20:50:01 -0300

    fix: experiment with vite-plugin-singlefile and barbajs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,11 @@
-import storyblok from "@storyblok/astro";
-import basicSsl from "@vitejs/plugin-basic-ssl";
-import { defineConfig } from "astro/config";
 import vercelServerless from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   vite: {
-    plugins: [basicSsl()],
-    server: {
-      https: true,
-    },
+    plugins: [viteSingleFile()],
   },
-  integrations: [
-    storyblok({
-      accessToken: "qBlqZdNwjFb738diAycVNQtt",
-      components: {
-        page: "storyblok/page",
-        hero: "storyblok/hero",
-      },
-      apiOptions: {
-        region: "us",
-      },
-    }),
-  ],
   output: "server",
   adapter: vercelServerless(),
 });

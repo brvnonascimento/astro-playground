d436a647d47fcf48f6d0b44f52c2b3fdadc9ae6f
Bruno Nascimento <bruno.hn@icloud.com>
2023-03-14 21:07:49 -0300

    chore: test astro critters with barbajs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import vercelServerless from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
+import critters from "astro-critters";
 
 export default defineConfig({
   output: "server",
   adapter: vercelServerless(),
+  integrations: [critters()],
 });

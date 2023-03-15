13c5a054195f53819220663e28f9ae88edec8aff
Bruno Nascimento <bruno.hn@icloud.com>
2023-03-14 21:09:38 -0300

    chore: remove astro-critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import vercelServerless from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
-import critters from "astro-critters";
 
 export default defineConfig({
   output: "server",
   adapter: vercelServerless(),
-  integrations: [critters()],
 });

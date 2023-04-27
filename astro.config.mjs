e24a37d19ce656bef2d1977579facead476403d5
Bruno Nascimento <bruno.hn@icloud.com>
2023-04-27 07:14:32 -0300

    feat: add back storyblok integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,23 @@
 import vercelServerless from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "PUBLIC");
 
 export default defineConfig({
   output: "server",
   adapter: vercelServerless(),
+  integrations: [
+    storyblok({
+      accessToken: env.PUBLIC_STORYBLOK_ACCESS_TOKEN,
+      components: {
+        page: "storyblok/page",
+        hero: "storyblok/hero",
+      },
+      apiOptions: {
+        region: "us",
+      },
+    }),
+  ],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="@astrojs/image/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_STORYBLOK_ACCESS_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}

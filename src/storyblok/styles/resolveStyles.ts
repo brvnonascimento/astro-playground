d81bce7dbec6adeb32cfc942f8c7cb2667e1b86b
Bruno Nascimento <bruno.hn@icloud.com>
2023-05-04 16:42:15 -0300

    feat: make the resolveStylesBlocks and .autoLayout class to also account for Alignment

--- src/storyblok/styles/resolveStyles.ts
+++ src/storyblok/styles/resolveStyles.ts
@@ -8,13 +8,23 @@ export const resolveStylesBlocks = (styles?: AutoLayoutStoryblok[]) => {
   }
 
   styles.forEach((style) => {
-    const { component, layoutOnMobile, layoutOnTablet, layoutOnDesktop } =
-      style;
+    const {
+      component,
+      layoutOnMobile,
+      layoutOnTablet,
+      layoutOnDesktop,
+      alignmentOnMobile,
+      alignmentOnTablet,
+      alignmentOnDesktop,
+    } = style;
 
     classNames += `${component} `;
-    classNames += `${layoutOnMobile} `;
-    classNames += `tablet-${layoutOnTablet} `;
-    classNames += `desktop-${layoutOnDesktop} `;
+    if (layoutOnMobile) classNames += `${layoutOnMobile} `;
+    if (layoutOnTablet) classNames += `tablet-${layoutOnTablet} `;
+    if (layoutOnDesktop) classNames += `desktop-${layoutOnDesktop} `;
+    if (alignmentOnMobile) classNames += `${alignmentOnMobile} `;
+    if (alignmentOnTablet) classNames += `tablet-${alignmentOnTablet} `;
+    if (alignmentOnDesktop) classNames += `desktop-${alignmentOnDesktop} `;
   });
 
   return { classNames: classNames.trim() };
